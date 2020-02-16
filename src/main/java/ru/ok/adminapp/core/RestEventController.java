60b82284fbaf79f0b6a806bbf0011dfedfaf66d8
Nikita Golikov <golikovnik@gmail.com>
2020-02-16 13:46:10 +0300

    Initial commit

---
+++ src/main/java/ru/ok/adminapp/core/RestEventController.java
@@ -0,0 +1,27 @@
+package ru.ok.adminapp.core;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestEventController {
+    final static private String BAN_URI = "/bot-api/ban";
+    final static private String EVENTS_URI = "/admin/events";
+
+    @PostMapping(BAN_URI)
+    public ResponseEntity processBan(@RequestBody Ban ban) {
+        System.err.printf("Post %d, comment %d banned\n",
+                ban.getPostId(), ban.getCommentId());
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+    @PostMapping(EVENTS_URI)
+    public ResponseEntity processEvent(@RequestBody Event event) {
+        new ServiceHandler().process(event, "http://localhost:8080" +
+                BAN_URI);
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+}
---
+++ src/main/java/ru/ok/adminapp/core/ServiceHandler.java
@@ -0,0 +1,60 @@
+package ru.ok.adminapp.core;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+public class ServiceHandler {
+    final private String WORD_DICT = "badWords.txt";
+
+    void addWord(String word) {
+        try (BufferedWriter wr = new BufferedWriter(new FileWriter(WORD_DICT, true))) {
+            wr.write(word + "\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void deleteWord(String word) {
+        List<String> lines;
+        try (BufferedReader br = new BufferedReader(
+                new FileReader(WORD_DICT))) {
+            lines = br.lines().filter(s -> !s.equals(word)).collect(Collectors.toList());
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+        try (BufferedWriter wr = new BufferedWriter(new FileWriter(WORD_DICT))) {
+            for (String line : lines) {
+                wr.write(line + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void process(Event event, String botApiURI) {
+        if (!isBad(event.getText())) {
+            return;
+        }
+
+        RestTemplate restTemplate = new RestTemplate();
+        HttpEntity<Ban> request = new HttpEntity<>(new Ban(
+                event.getPostId(), event.getCommentId()
+        ));
+        restTemplate.postForLocation(botApiURI, request, Ban.class);
+    }
+
+    private boolean isBad(String text) {
+        try (BufferedReader br = new BufferedReader(
+                new FileReader(WORD_DICT))) {
+            return br.lines().anyMatch(text::contains);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
