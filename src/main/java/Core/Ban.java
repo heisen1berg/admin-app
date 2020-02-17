434db4491092b6eb163a856b7b1ccd3beb47dddf
heisen1berg <rybtsov_i_v@mail.ru>
2020-02-17 05:50:24 +0300

    Merged build

--- src/main/java/Core/Ban.java
+++ src/main/java/Core/Ban.java
@@ -1,4 +1,4 @@
-package ru.ok.adminapp.core;
+package Core;
 
 public class Ban {
     private long postId;
--- src/main/java/Core/Event.java
+++ src/main/java/Core/Event.java
@@ -1,4 +1,4 @@
-package ru.ok.adminapp.core;
+package Core;
 
 public class Event {
     private long postId;
